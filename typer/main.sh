9422e6051a5ed488fe7451b6bc594f0d50213d18
Vladyslav Latsko <vladislav.lacko@gmail.com>
2023-12-03 14:45:35 +0100

    few words

--- typer/main.sh
+++ typer/main.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
-alphabet=({a..z})
-arr_length=${#alphabet[@]}
+frequent_char_sets=("th" "he" "in" "er" "an" "re" "ed" "on" "es" "st" "en" "at" "to" "nt" "ha" "ou" "ea" "ng" "ing" "igh" "ough" "tion" "tio" "ter" "est")
+arr_length=${#frequent_char_sets[@]}
 
 get_random_index() {
 	echo $((RANDOM % $arr_length))
 }
 
-get_random_index
-get_random_index
-get_random_index
+# add_set() {
+# 	echo "${frequent_char_sets[$(get_random_index)]}"
+# }
 
+add_set() {
+    local random_index
+    local result=""
+    local num_words=$1  # Number of words specified by the user
 
-echo -ne ${alphabet[$(get_random_index)]}
-echo -ne ${alphabet[$(get_random_index)]}
-echo -ne ${alphabet[$(get_random_index)]}
-echo -ne ${alphabet[$(get_random_index)]}
-echo
+    for _ in $(seq "$num_words"); do
+        random_index=$(get_random_index)
+        result+="${frequent_char_sets[$random_index]} "
+    done
 
+    echo "$result"
+}
+
+uno=$(add_set 3)
+echo $uno
+
+echo "Type the word above:"
 read typed_text
 
-echo $typed_text
-# echo $arr_length
-# echo ${alphabet[*]}
+if [ "$typed_text" == "$uno" ] 
+then
+	echo "You got it!"
+else
+	echo "Try again !"
+fi
+
 
