739ff664f95920a6f89612a33074376b7b02b636
Vladyslav Latsko <vladislav.lacko@gmail.com>
2023-12-03 14:51:48 +0100

    This modification uses parameter expansion () to remove the trailing space from the generated words, and it ensures that both the generated words and the user input are compared without trailing spaces.

--- typer/main.sh
+++ typer/main.sh
@@ -3,6 +3,9 @@
 frequent_char_sets=("th" "he" "in" "er" "an" "re" "ed" "on" "es" "st" "en" "at" "to" "nt" "ha" "ou" "ea" "ng" "ing" "igh" "ough" "tion" "tio" "ter" "est")
 arr_length=${#frequent_char_sets[@]}
 
+echo "How many words ?"
+read word_count
+
 get_random_index() {
 	echo $((RANDOM % $arr_length))
 }
@@ -14,7 +17,7 @@ get_random_index() {
 add_set() {
     local random_index
     local result=""
-    local num_words=$1  # Number of words specified by the user
+    local num_words=$word_count  # Number of words specified by the user
 
     for _ in $(seq "$num_words"); do
         random_index=$(get_random_index)
@@ -24,17 +27,33 @@ add_set() {
     echo "$result"
 }
 
-uno=$(add_set 3)
+uno=$(add_set 6)
 echo $uno
 
 echo "Type the word above:"
-read typed_text
 
-if [ "$typed_text" == "$uno" ] 
-then
-	echo "You got it!"
+read -r typed_text  # Use -r to preserve backslashes
+
+# Remove trailing spaces from both generated words and user input
+uno_trimmed="${uno%" "}"
+typed_text_trimmed="${typed_text%" "}"
+
+# Compare the trimmed strings
+if [ "$typed_text_trimmed" == "$uno_trimmed" ]; then
+    echo "You got it!"
 else
-	echo "Try again !"
+    echo "Try again!"
 fi
+
+
+
+# read typed_text
+
+# if [ "$typed_text" == "$uno" ] 
+# then
+# 	echo "You got it!"
+# else
+# 	echo "Try again !"
+# fi
 
 
